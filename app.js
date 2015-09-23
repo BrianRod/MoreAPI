594929d1314be770a1e1f35df9f46b59cb27eafb
Daniel Treasure <dbtreasure@gmail.com>
2015-09-23 15:44:37 -0600

    places bus #1 busses as markers on map

--- app.js
+++ app.js
@@ -4,21 +4,30 @@
 
 //#2 Provide a callback function to construct the map, and place it on the DOM
 
-//#3 Find the busses,
+//#3 Request realtime bus locations,
 
 //#4 Take bus locations, and make markers out of them
 
-var map;
-
 function initMap(){
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 3,
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 64.135491, lng: -21.896149},
+        zoom: 10,
         mapTypeId: google.maps.MapTypeId.HYBRID
     });
-    console.log('initMap')
-    var url = "http://apis.is/bus/realtime";
-    $.get(url, function(data){
+
+    $.get("http://apis.is/bus/realtime", function(data){
         console.log(data)
-    })
-}
+        var busnmbr1 = data.results[0];
+        busnmbr1.busses.forEach(function(bus){
+            var busLocale = {
+                lat: bus.x,
+                lng: bus.y
+            };
+            var marker = new google.maps.Marker({
+                map: map,
+                animation: google.maps.Animation.DROP,
+                position: busLocale
+            });
+        });
+    });
+};
