6894bfa729aadbf4e356217aa1d534eed8665768
Daniel Treasure <dbtreasure@gmail.com>
2015-09-23 15:13:49 -0600

    adds jquery

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+//STEPS
+
+//#1 Grab the Google Map from google (we did this in the HTML)
+
+//#2 Provide a callback function to construct the map, and place it on the DOM
+
+//#3 Find the busses,
+
+//#4 Take bus locations, and make markers out of them
+
+var map;
+
 function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 3,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    });
     console.log('initMap')
+    var url = "http://apis.is/bus/realtime";
 }
